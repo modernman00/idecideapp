80040af3551498004ae7da517f4cb6035cdee222
modernman00 <modernman00@yahoo.com>
2026-06-05 00:55:20 +0100

    refactor: remove background synchronization logic and local IndexedDB store from result assets

--- public/js/createBlog.js
+++ public/js/createBlog.js
@@ -1,26 +1,2 @@
-"use strict";
-(self["webpackChunkidecide"] = self["webpackChunkidecide"] || []).push([["createBlog"],{
-
-/***/ "./resources/assets/js/createBlog.js":
-/*!*******************************************!*\
-  !*** ./resources/assets/js/createBlog.js ***!
-  \*******************************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modernman00_shared_js_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @modernman00/shared-js-lib */ "./node_modules/@modernman00/shared-js-lib/index.js");
-//import axios 
-
-(0,_modernman00_shared_js_lib__WEBPACK_IMPORTED_MODULE_0__.registerHandler)({
-  formId: "createBlog",
-  route: 'createBlog',
-  redirect: 'blogs',
-  optionalFields: ['blogImg'],
-  theme: 'bulma'
-});
-(0,_modernman00_shared_js_lib__WEBPACK_IMPORTED_MODULE_0__.showFileName)('blogImg_div');
-
-/***/ })
-
-}]);
+"use strict";(self.webpackChunkidecide=self.webpackChunkidecide||[]).push([[659],{805:function(e,i,l){l.r(i);var o=l(71);(0,o.ik)({formId:"createBlog",route:"createBlog",redirect:"blogs",optionalFields:["blogImg"],theme:"bulma"}),(0,o.FU)("blogImg_div")}}]);
 //# sourceMappingURL=createBlog.js.map
--- public/js/register.js
+++ public/js/register.js
@@ -1,25 +1,2 @@
-"use strict";
-(self["webpackChunkidecide"] = self["webpackChunkidecide"] || []).push([["register"],{
-
-/***/ "./resources/assets/js/acctMgt/register.js":
-/*!*************************************************!*\
-  !*** ./resources/assets/js/acctMgt/register.js ***!
-  \*************************************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modernman00_shared_js_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @modernman00/shared-js-lib */ "./node_modules/@modernman00/shared-js-lib/index.js");
-//import axios 
-
-(0,_modernman00_shared_js_lib__WEBPACK_IMPORTED_MODULE_0__.registerHandler)({
-  formId: "regForm",
-  route: 'register',
-  redirect: 'login',
-  optionalFields: ['confirm_password'],
-  recaptchaAction: 'REGISTER'
-});
-
-/***/ })
-
-}]);
+"use strict";(self.webpackChunkidecide=self.webpackChunkidecide||[]).push([[224],{406:function(e,i,r){r.r(i),(0,r(71).ik)({formId:"regForm",route:"register",redirect:"login",optionalFields:["confirm_password"],recaptchaAction:"REGISTER"})}}]);
 //# sourceMappingURL=register.js.map
